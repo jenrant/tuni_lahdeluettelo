42c66cc5b3b3fc74fd9cf5e78d69cd9613746600
Jenni Rantanen <jenrant@protonmail.com>
2023-09-14 17:46:18 +0300

    fixed .bat file

--- styles/Lähdeluettelon asennus.bat
+++ styles/Lähdeluettelon asennus.bat
@@ -4,8 +4,7 @@ set location=%cd%
 copy "TUNI Lähdeluettelo.xsl" "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\TUNI Lähdeluettelo.xsl"
 if exist "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\TUNI Lähdeluettelo.xsl" (
 echo "Lähdeluettelon tyylitiedosto kopioitu."
-)
-else (
+) else (
 echo "Lähdeluettelon tyylitiedoston kopioinnissa tapahtui virhe."
 )
 pause
