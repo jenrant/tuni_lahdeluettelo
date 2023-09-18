24893d312c4c71b0b9b369eaf3332580544adcb8
Jenni Rantanen <jenrant@protonmail.com>
2023-09-18 15:26:20 +0300

    updated installer batch file to check if the bibliography folder exists

--- styles/Lähdeluettelon asennus.bat
+++ styles/Lähdeluettelon asennus.bat
@@ -1,10 +1,41 @@
 @echo off
 @chcp 65001
 set location=%cd%
-copy "TUNI Lähdeluettelo.xsl" "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\TUNI Lähdeluettelo.xsl"
-if exist "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\TUNI Lähdeluettelo.xsl" (
-echo "Lähdeluettelon tyylitiedosto kopioitu."
+echo .
+
+REM Check if the parent folder for the bibliography style exists
+if exist "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\" (
+
+REM If it does not exist create it
 ) else (
-echo "Lähdeluettelon tyylitiedoston kopioinnissa tapahtui virhe."
+    mkdir "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\"
+    echo Luotiin kansio "Bibliography"
 )
+
+REM Check if the folder for the bibliography style exists
+if exist "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\" (
+
+REM If the folder does not exist create it
+) else (
+    mkdir "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\"
+    echo Luotiin kansio "Style"
+)
+
+echo .
+
+REM Try to copy the bibliography style
+copy "TUNI Lähdeluettelo.xsl" "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\TUNI Lähdeluettelo.xsl"
+
+echo .
+
+REM Check if the copy operation was successful
+if exist "C:\users\%username%\AppData\Roaming\Microsoft\Bibliography\Style\TUNI Lähdeluettelo.xsl" (
+echo Lähdeluettelon tyylitiedosto kopioitu.
+
+REM Display an error message if the operation failed
+) else (
+echo Lähdeluettelon tyylitiedoston kopioinnissa tapahtui virhe.
+)
+
+echo .
 pause
